94e20df548f87b6179daa6379de0f09b66133d42
salvatoluice <salvatoluice5@gmail.com>
2024-08-01 08:56:42 +0300

    add

--- src/Merchant/Orders.jsx
+++ src/Merchant/Orders.jsx
@@ -35,20 +35,20 @@ const Orders = () => {
     <Layout>
       <div className="flex items-center mb-4 border-b-2 border-gray-200">
         <button
-          className={`py-2 px-4 ${activeTab === 'all' ? 'text-primary border-b-2 border-primary' : 'text-gray-600'}`}
+          className={`py-2 px-4 text-[14px] ${activeTab === 'all' ? 'text-primary border-b-2 border-primary' : 'text-gray-600'}`}
           onClick={() => setActiveTab('all')}
         >
           All Orders
         </button>
         <button
-          className={`py-2 px-4 ml-2 ${activeTab === 'pending' ? 'bg-white text-primary border-b-2 border-primary' : 'text-gray-600'}`}
+          className={`py-2 px-4 ml-2 text-[14px] ${activeTab === 'pending' ? 'bg-white text-primary border-b-2 border-primary' : 'text-gray-600'}`}
           onClick={() => setActiveTab('pending')}
         >
           Pending Orders
         </button>
       </div>
       <div className="overflow-x-auto bg-gray-50 px-4">
-        <div className="flex items-center justify-between w-full pl-4 h-[35px] rounded-md">
+        <div className="flex items-center justify-between w-full pl-4 mt-2 h-[35px] rounded-md">
           <p className="text-black font-medium text-[16px]">Orders</p>
           <div className="w-[180px] py-2 flex items-center bg-white px-2 rounded-full h-full">
             <input type="text" placeholder='Search...' className='w-[140px] px-2 h-full' />
@@ -57,13 +57,13 @@ const Orders = () => {
         <table className="min-w-full">
           <thead>
             <tr>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Order ID</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Merchant ID</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Customer ID</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Total</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Discount</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Points</th>
-              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[15px] border-gray-300">Actions</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Order ID</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Merchant ID</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Customer ID</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Total</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Discount</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Points</th>
+              <th className="py-3 px-4 border-b text-gray-700 text-start font-medium text-[14px] border-gray-300">Actions</th>
             </tr>
           </thead>
           <tbody>
